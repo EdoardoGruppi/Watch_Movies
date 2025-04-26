3eb72d3518ba8c30dd28557581cb42392458a680
EdoardoGruppi <edoardogruppi@gmail.com>
2025-04-26 18:33:40 +0000

    🔧 Create constants interfaces

---
+++ src/constants/services.ts
@@ -0,0 +1,12 @@
+export const SERVICES = new Set<string>([
+  "Google Play Movies",
+  "Apple TV",
+  "Amazon Video",
+  "Microsoft Store",
+  "YouTube",
+  "Rakuten TV",
+  "Sky Store",
+  "Canal+",
+  "Movistar Plus+",
+  "Netflix",
+]);
