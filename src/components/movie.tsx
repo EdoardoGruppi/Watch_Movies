49879b897c6eb044740c8dd855f174c9d867c778
EdoardoGruppi <edoardogruppi@gmail.com>
2025-04-26 18:34:14 +0000

    💣  Show data inside table

--- src/components/movie.tsx
+++ src/components/movie.tsx
@@ -12,12 +12,20 @@ import {
   Title,
 } from "@mantine/core";
 import { format } from "date-fns";
-import { useContext, useMemo, useState } from "react";
+import { useContext, useMemo } from "react";
 import imdbLogo from "@assets/imdb.svg";
 import tmdbLogo from "@assets/tmdb.svg";
 import rottenLogo from "@assets/rottentomato.svg";
-import { AllCommunityModule, ModuleRegistry, ColDef } from "ag-grid-community";
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  ColDef,
+  ICellRendererParams,
+} from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
+import { Cell, Row } from "@interfaces/table";
+import { SERVICES } from "@constants/services";
+import { COUNTRIES } from "@constants/countries";
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
@@ -25,20 +33,73 @@ ModuleRegistry.registerModules([AllCommunityModule]);
 export function Movie() {
   const { movies, selected: id, offers } = useContext(BaseContext);
   const movie = useMemo(() => movies!.find((i) => i.id === id), [id, movies]);
-
-  //   const [rowData, setRowData] = useState([
-  //     { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-  //     { make: "Ford", model: "F-Series", price: 33850, electric: false },
-  //     { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  //   ]);
-  //   const [colDefs, setColDefs] = useState<
-  //     ColDef<{ make: string; model: string; price: number; electric: boolean }>[]
-  //   >([
-  //     { field: "make" },
-  //     { field: "model" },
-  //     { field: "price" },
-  //     { field: "electric" },
-  //   ]);
+  const rowData = useMemo(
+    () =>
+      Object.entries(offers).map(([key, val]) => {
+        const row = { country: COUNTRIES[key] } as Row;
+        val.forEach((offer) => {
+          let service = offer.package.name;
+          if (service.startsWith("Apple TV")) service = "Apple TV";
+          if (SERVICES.has(service)) {
+            if (
+              (row[service] === undefined ||
+                offer.priceValue === null ||
+                (row[service] as Cell).price === undefined ||
+                (row[service] as Cell).price) ??
+              0 > offer.priceValue
+            ) {
+              row[service] = {
+                price: offer.priceValue,
+                currency: offer.priceCurrency,
+                languages: offer.audioLanguages,
+                subtitles: offer.subtitleLanguages,
+                url: offer.url,
+              };
+            }
+          }
+        });
+        return row;
+      }),
+    [offers]
+  );
+  const columnDefs: ColDef[] = [
+    {
+      field: "country",
+      headerName: "Country",
+      pinned: "left",
+      filter: true,
+      comparator: (a: string, b: string) => {
+        const cleanA = a.slice(5).trim();
+        const cleanB = b.slice(5).trim();
+        return cleanA.localeCompare(cleanB);
+      },
+    },
+    ...Array.from(SERVICES).map((serviceName) => ({
+      field: serviceName,
+      headerName: serviceName,
+      autoHeight: true,
+      wrapText: true,
+      cellRenderer: (params: ICellRendererParams) => {
+        const data = params.value;
+        if (!data) return "";
+        const priceDisplay =
+          data.price == null || data.currency == null
+            ? "Free"
+            : `${data.price} ${data.currency}`;
+        if (data.url)
+          return (
+            <a
+              href={params.value.url}
+              target="_blank"
+              style={{ textDecoration: "none" }}
+            >
+              {priceDisplay}
+            </a>
+          );
+        return priceDisplay;
+      },
+    })),
+  ];
 
   return (
     <>
@@ -240,33 +301,39 @@ export function Movie() {
                       verticalSpacing={1}
                       withColumnBorders
                     >
-                      <Table.Tr>
-                        <Table.Th>Top</Table.Th>
-                        <Table.Th>Days</Table.Th>
-                      </Table.Tr>
-                      <Table.Tr>
-                        <Table.Td>{3}</Table.Td>
-                        <Table.Td>{movie.streamingCharts?.daysInTop3}</Table.Td>
-                      </Table.Tr>
-                      <Table.Tr>
-                        <Table.Td>{10}</Table.Td>
-                        <Table.Td>
-                          {movie.streamingCharts?.daysInTop10}
-                        </Table.Td>
-                      </Table.Tr>
-                      <Table.Tr>
-                        <Table.Td>{100}</Table.Td>
-                        <Table.Td>
-                          {movie.streamingCharts?.daysInTop100}
-                        </Table.Td>
-                      </Table.Tr>
+                      <Table.Thead>
+                        <Table.Tr>
+                          <Table.Th>Top</Table.Th>
+                          <Table.Th>Days</Table.Th>
+                        </Table.Tr>
+                      </Table.Thead>
+                      <Table.Tbody>
+                        <Table.Tr>
+                          <Table.Td>{3}</Table.Td>
+                          <Table.Td>
+                            {movie.streamingCharts?.daysInTop3}
+                          </Table.Td>
+                        </Table.Tr>
+                        <Table.Tr>
+                          <Table.Td>{10}</Table.Td>
+                          <Table.Td>
+                            {movie.streamingCharts?.daysInTop10}
+                          </Table.Td>
+                        </Table.Tr>
+                        <Table.Tr>
+                          <Table.Td>{100}</Table.Td>
+                          <Table.Td>
+                            {movie.streamingCharts?.daysInTop100}
+                          </Table.Td>
+                        </Table.Tr>
+                      </Table.Tbody>
                     </Table>
                   </div>
                 </div>
               </div>
             </Card>
           </Group>
-          {/* <div
+          <div
             className="ag-theme-quartz"
             style={{
               height: 500,
@@ -281,9 +348,9 @@ export function Movie() {
               colResizeDefault="shift"
               defaultColDef={{ flex: 1 }}
               rowData={rowData}
-              columnDefs={colDefs}
+              columnDefs={columnDefs}
             />
-          </div> */}
+          </div>
         </>
       )}
     </>
